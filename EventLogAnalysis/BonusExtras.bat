3331719368c42958154a725b3d6f6bead5ac2b72
jayers <jayers@jaMacBook.mycroft.xxx>
2018-04-19 00:45:38 -0700

    ntc

--- EventLogAnalysis/BonusExtras.bat
+++ EventLogAnalysis/BonusExtras.bat
@@ -3,8 +3,10 @@ rem Events/ev:Event[ev:System/ev:Provider/@Name='MSSQLSERVER' and ev:System/ev:E
 
 for %i in (System, Application) do wevtutil qe %i /r:SOMESERVERNAME /q:"*[System[(Level=1 or Level=2 or Level=3) and TimeCreated[@SystemTime>='2014-07-01T08:00:00.000Z']]]" >>
 
-wevtutil qe Application /q:"*[System[(Level=1 or Level=2 or Level=3) and TimeCreated[@SystemTime>='2014-01-01T08:00:00.000Z']]]"
+rem syntax
+wevtutil {qe | query-events} <Path> [/lf:<Logfile>] [/sq:<Structquery>] [/q:<Query>] [/bm:<Bookmark>] [/sbm:<Savebm>] [/rd:<Direction>] [/f:<Format>] [/l:<Locale>] [/c:<Count>] [/e:<Element>]
 
+rem example
+wevtutil qe Application /q:"*[System[(Level=1 or Level=2 or Level=3) and TimeCreated[@SystemTime>='2014-01-01T08:00:00.000Z']]]"
 wevtutil qe Application /q:"*[System[(Level=1 or Level=2 or Level=3) and (EventID!=1111 or EventID!=56 or EventID!=7034 or EventID!=36 or EventID!=1530 or EventID!=47) and TimeCreated[@SystemTime>='2014-01-01T08:00:00.000Z']]]"
 
-wevtutil {qe | query-events} <Path> [/lf:<Logfile>] [/sq:<Structquery>] [/q:<Query>] [/bm:<Bookmark>] [/sbm:<Savebm>] [/rd:<Direction>] [/f:<Format>] [/l:<Locale>] [/c:<Count>] [/e:<Element>]
